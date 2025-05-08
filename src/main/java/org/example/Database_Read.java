66c65648d95d1601a0d80b9792a095fb97d6c9d4
chawsusuthwe <chawsu2422@gmail.com>
2025-05-08 14:23:35 +0630

    Offices and Database Read

--- src/main/java/org/example/Database_Read.java
+++ src/main/java/org/example/Database_Read.java
@@ -3,6 +3,7 @@ package org.example;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class Database_Read {
     private Connection conn;
@@ -11,17 +12,23 @@ public class Database_Read {
         this.conn = conn;
     }
 
-    protected void read(){
+    protected ArrayList<Offices> read(){
+        ArrayList<Offices> aloffice = new ArrayList<>();
         try{
             PreparedStatement ps = conn.prepareStatement("SELECT * FROM offices WHERE country = ?");
             //PreparedStatement ps = conn.preparedStatement("SELECT * FROM offices WHERE country = ?");
             ps.setString(1,"USA");
             ResultSet rs = ps.executeQuery();
+
+
             while(rs.next()){
-                System.out.print(rs.getInt(1));
-                System.out.print("|");
-                System.out.print(rs.getString("city"));
-                System.out.println();
+//                System.out.print(rs.getInt(1));
+//                System.out.print("|");
+//                System.out.print(rs.getString("city"));
+//                System.out.println();
+                aloffice.add(new Offices(rs.getString(1),rs.getString(2),rs.getString(3),
+                        rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7),
+                        rs.getString(8),rs.getString(9)));
             }
 
             rs.close();
@@ -30,5 +37,10 @@ public class Database_Read {
         catch(Exception e){
             e.printStackTrace();
         }
+        return aloffice;
+    }
+
+    protected void display(ArrayList<Offices> aloffice){
+
     }
 }
